b1a4589a05a3181b3450d7a1fa0afa5b46293cd2
Trovoada01 <112417747+Trovoada01@users.noreply.github.com>
2023-09-11 14:51:13 -0300

    Add files via upload

---
+++ Wagner-28-06-main/js/calcula.js
@@ -0,0 +1,22 @@
+// Capturar os dados do formulário
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function(e) {e.preventDefault();
+
+  const n1 = form.querySelector('#Num1');
+  const n2 = form.querySelector('#Num2');
+
+  const n1v = Number(n1.value);
+  const n2v = Number(n2.value);
+
+  function media(media);
+    const med = n1v * n2v / 2
+    return med
+
+
+
+
+
+
+
+})
